a0ddb184f382056b5d21d9a7322eea3aca64a12a
855474 <855474@SAIT227642.ACDM.DS.SAIT.CA>
2021-06-08 19:24:44 -0600

    Second Commit User is now able to save edits and re-navigate to either view or edit pages. Added text population when inputs are empty.

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -21,9 +21,10 @@ public class NoteServlet extends HttpServlet {
         String inFile_contents = "";
         String path = getServletContext().getRealPath("/WEB-INF/note.txt");
         
-        // to read files
+        // Reads the contents of a file
         BufferedReader br = new BufferedReader(new FileReader(new File(path)));
         
+        // stores the lines of the file
         inFile_title += br.readLine();
         inFile_contents += br.readLine();
         
@@ -34,11 +35,11 @@ public class NoteServlet extends HttpServlet {
         if (getEdit != null) {
             // this will display the requested JSP as a view
             getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp").forward(request, response);
-        } else {
-            // this will display the requested JSP as a view
-            getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
+        }else{
+           // this will display the requested JSP as a view
+            getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response); 
         }
-        
+        // close the stream
         br.close();
     }
 
@@ -46,5 +47,28 @@ public class NoteServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        // Writes content to the file
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, false))); 
+
+        String title_area = request.getParameter("title_area");
+        String content_area = request.getParameter("content_area");
+        
+        // if inputs are empty, populate with predetermined message
+        if(title_area == null || title_area.equals("")){
+            title_area = "Please insert title";
+        }else if(content_area == null || content_area.equals("")){
+            content_area = "Please insert content";
+        }
+        
+        pw.println(title_area);
+        pw.println(content_area);
+        // close the stream
+        pw.close();
+        
+        Note note = new Note(title_area, content_area);
+        request.setAttribute("note", note);
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
     }
 }
